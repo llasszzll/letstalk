e6f1f15b3e36e9a361503c63432874a1013a05b3
llasszzll@gmail.com <llasszzll@gmail.com>
2025-02-24 18:46:28 +0200

    Initial Design

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const fs = require('fs')
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressLayout = require('express-ejs-layouts');
+
+// const connectDB = require('./server/config/db')
+
+const app = express();
+const PORT = 5000 || process.env.PORT
+
+// CONNECT TO DB
+// connectDB();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Template engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+app.use('/', require('./server/routes/main'));
+
+app.listen(PORT, () => {
+    console.log(`App Listening on port ${PORT}`);
+})
---
+++ server/routes/main.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+// const Post = require('../models/post')
+
+
+// * GET 
+// * HOME
+
+router.get('', async (req, res) => {
+    const locals = {
+        title: "Home",
+        description: "Let's Talk Solution"
+    }
+    try {
+        // const data = await Post.find();
+        res.render('index', { locals });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// data
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/blog', (req, res) => {
+    res.render('blog');
+});
+
+router.get('/service', (req, res) => {
+    res.render('service');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+module.exports = router;
